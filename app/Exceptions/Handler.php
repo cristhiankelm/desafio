73397f5fd95963d570af4ec782519c24ebe8d2d8
Cristhian <criskp007@gmail.com>
2021-11-21 00:09:21 -0300

    Product Controller

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,6 +2,7 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
@@ -48,8 +49,16 @@ class Handler extends ExceptionHandler
      *
      * @throws \Throwable
      */
-    public function render($request, Throwable $exception)
+    public function render($request, Exception $exception)
     {
+        // This will replace our 404 response with
+        // a JSON response.
+        if ($exception instanceof ModelNotFoundException) {
+            return response()->json([
+                'error' => 'Resource not found'
+            ], 404);
+        }
+
         return parent::render($request, $exception);
     }
 }
--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Category;
+use App\Helpers\APIHelpers;
 use App\Product;
 use Illuminate\Http\Request;
+use App\Http\Requests\Product as ProductRequest;
 
 class ProductController extends Controller
 {
@@ -17,7 +19,8 @@ class ProductController extends Controller
     {
         $products = Product::all();
 
-        return response()->json($products);
+        $response = APIHelpers::createAPIResponse(false, 200, '', $products);
+        return response()->json($response, 200);
     }
 
     /**
@@ -26,7 +29,7 @@ class ProductController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
         $product = new Product();
 
@@ -38,8 +41,15 @@ class ProductController extends Controller
         $product->created_at = date(now());
         $product->updated_at = date(now());
 
-        $product->save();
-        return response()->json(['message' => 'Registered successfully']);
+        $productSave = $product->save();
+
+        if ($productSave) {
+            $response = APIHelpers::createAPIResponse(false, 201, 'Product added successfully', null);
+            return response()->json($response, 201);
+        } else {
+            $response = APIHelpers::createAPIResponse(true, 400, 'Product creation failed', null);
+            return response()->json($response, 400);
+        }
     }
 
     /**
@@ -50,7 +60,10 @@ class ProductController extends Controller
      */
     public function show($id)
     {
-        //
+        $product = Product::find($id);
+
+        $response = APIHelpers::createAPIResponse(false, 200, '', $product);
+        return response()->json($response, 200);
     }
 
     /**
@@ -60,9 +73,26 @@ class ProductController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(ProductRequest $request, $id)
     {
-        //
+        $product = Product::find($id);
+
+        $product->category_id = $request->category_id;
+        $product->name = $request->name;
+        $product->price = $request->price;
+        $product->inventory = $request->inventory;
+        $product->status = $request->status;
+        $product->updated_at = date(now());
+
+        $productSave = $product->save();
+
+        if ($productSave) {
+            $response = APIHelpers::createAPIResponse(false, 200, 'Product updated successfully', null);
+            return response()->json($response, 200);
+        } else {
+            $response = APIHelpers::createAPIResponse(true, 400, 'Product update failed', null);
+            return response()->json($response, 400);
+        }
     }
 
     /**
@@ -73,6 +103,15 @@ class ProductController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $product = Product::find($id);
+        $productDelete = $product->delete();
+
+        if ($productDelete) {
+            $response = APIHelpers::createAPIResponse(false, 200, 'Product deleted successfully', null);
+            return response()->json($response, 200);
+        } else {
+            $response = APIHelpers::createAPIResponse(true, 400, 'Product delete failed', null);
+            return response()->json($response, 400);
+        }
     }
 }
